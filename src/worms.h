306a8903591e57b0c23c4fbd34b9a17eff518257
Lukas Weber <lweber@physik.rwth-aachen.de>
2022-03-04 12:01:22 +0100

    go back to xor worms for now: for some reason the loop update does not work with add worms
    i think the solver chooses a solution where the matrix elements for jdim correlations are never reached

--- src/worms.h
+++ src/worms.h
@@ -4,13 +4,24 @@
 
 // Implementation of the worms. Choice is largely irrelevant as long as it is bijective in a certain sense.
 using worm_idx = int;
-
+/*
 inline state_idx worm_action(worm_idx worm, state_idx state, int basis_size) {
 	return (state + worm + 1) % basis_size;
 }
 
 inline worm_idx worm_inverse(worm_idx worm, int basis_size) {
 	return basis_size - worm - 2;
+}
+
+inline int worm_count(int basis_size) {
+	return basis_size - 1;
+}*/
+inline state_idx worm_action(worm_idx worm, state_idx state, int basis_size) {
+	return state^(worm + 1);
+}
+
+inline worm_idx worm_inverse(worm_idx worm, int basis_size) {
+	return worm;
 }
 
 inline int worm_count(int basis_size) {
