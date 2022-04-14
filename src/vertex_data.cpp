88a4215b2050b9e17ddf34d537d44984eb6796ce
Lukas Weber <lweber@physik.rwth-aachen.de>
2022-04-14 15:43:31 +0200

    vertex_data: more effective safeguard against inaccurate lp solutions

--- src/vertex_data.cpp
+++ src/vertex_data.cpp
@@ -253,18 +253,25 @@ vertex_data::vertex_data(const std::vector<int> &dims, const Eigen::MatrixXd &bo
 
 	for(auto &t : transitions_) {
 		int idx{};
+		double norm{};
 		for(auto &p : t.probs) {
 			if(p > 0 && t.targets[idx].invalid()) {
 				t.print();
 				throw std::runtime_error{
 				    fmt::format("it is possible to reach an invalid vertex with p={}", p)};
 			}
+			norm += p;
 			idx++;
+		}
+		if(norm > 0) {
+			assert(fabs(norm - 1) <
+			       1e-7); // as long as the norm is not completely off, renormalize.
+			for(auto &p : t.probs) {
+				p /= norm;
+			}
 		}
 
 		std::partial_sum(t.probs.begin(), t.probs.end(), t.probs.begin());
-		assert(t.probs.back() < 1.0000000001);
-		// assert(!t.invalid());
 	}
 }
 
