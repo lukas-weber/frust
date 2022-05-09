dc2311df49c69e1e434e66185d329a170ecd5fa7
Lukas Weber <lweber@physik.rwth-aachen.de>
2022-05-09 10:53:14 +0200

    opercode: make 64 bit to accomodate larger bases

--- src/opercode.h
+++ src/opercode.h
@@ -5,81 +5,81 @@
 
 class vertexcode {
 public:
-	static const uint32_t maxbits = 4 * 3 + 1;
+	static const uint64_t maxbits = 8 * 3 + 1;
 	bool diagonal() const {
-		return code_ & 1;
+		return code_ & 1L;
 	}
 
 	vertexcode() = default;
 
-	explicit vertexcode(bool diagonal, uint32_t vertex_idx) : code_{diagonal | (vertex_idx << 1)} {
+	explicit vertexcode(bool diagonal, uint64_t vertex_idx) : code_{diagonal | (vertex_idx << 1)} {
 		assert(!invalid());
 	}
 
-	explicit vertexcode(uint32_t code) : code_{code} {}
+	explicit vertexcode(uint64_t code) : code_{code} {}
 
-	uint32_t vertex_idx() const {
-		return code_ >> 1;
+	uint64_t vertex_idx() const {
+		return code_ >> 1L;
 	}
 
-	uint32_t code() const {
+	uint64_t code() const {
 		return code_;
 	}
 
 	bool invalid() const {
-		return code_ > 1 << maxbits;
+		return code_ > 1L << maxbits;
 	}
 
 private:
-	uint32_t code_{(1 << maxbits) + 1};
+	uint64_t code_{(1L << maxbits) + 1L};
 };
 
 class opercode {
 public:
 	static opercode make_identity();
 
-	uint32_t code() const;
+	uint64_t code() const;
 	vertexcode vertex() const;
-	uint32_t bond() const;
+	uint64_t bond() const;
 
 	bool identity() const;
 	bool diagonal() const;
 
 	opercode() = default;
-	explicit opercode(uint32_t code) : code_{code} {}
-	explicit opercode(uint32_t bond, vertexcode vertex);
+	explicit opercode(uint64_t code) : code_{code} {}
+	explicit opercode(uint64_t bond, vertexcode vertex);
 
 private:
-	uint32_t code_{};
+	uint64_t code_{};
 };
 
 inline opercode opercode::make_identity() {
 	return opercode{0};
 }
 
-inline opercode::opercode(uint32_t bond, vertexcode vertex) {
-	uint32_t v = vertex.code();
+inline opercode::opercode(uint64_t bond, vertexcode vertex) {
+	uint64_t v = vertex.code();
 
-	assert(v < (1 << vertexcode::maxbits));
-	assert(bond < (1 << (8 * sizeof(code_) - vertexcode::maxbits - 1)));
+	assert(v < (1L << vertexcode::maxbits));
+	assert(bond < (1L << (8L * sizeof(code_) - vertexcode::maxbits - 1L)));
 
-	code_ = 1 | v << 1 | bond << (1 + vertexcode::maxbits);
+	code_ = 1L | v << 1L | bond << (1L + vertexcode::maxbits);
 }
 
-inline uint32_t opercode::code() const {
+inline uint64_t opercode::code() const {
 	return code_;
 }
 
-inline uint32_t opercode::bond() const {
-	return code_ >> (1 + vertexcode::maxbits);
+inline uint64_t opercode::bond() const {
+	return code_ >> (1L + vertexcode::maxbits);
 }
 
 inline vertexcode opercode::vertex() const {
-	return vertexcode{(code_ & ((1 << (1 + 4 * 3)) - 1)) >> 1};
+	return vertexcode{(code_ & ((1L << vertexcode::maxbits) - 1L)) >> 1L};
 }
 
 inline bool opercode::identity() const {
-	return code_ == 0;
+	return code_ == 0L;
 }
 
 inline bool opercode::diagonal() const {
