2c737a8351b563cc492b32ca4da4bd54914e259d
Lukas Weber <lweber@physik.rwth-aachen.de>
2022-05-17 13:56:35 +0200

    cavity_magnet: add columnar_dimer lattice & refactor lattice defs

--- src/models/cavity_magnet/definitions.cpp
+++ src/models/cavity_magnet/definitions.cpp
@@ -46,38 +46,29 @@ static std::vector<std::vector<double>> make_cavity(const unitcell &uc, const lo
 
 struct cavity_magnet_proto {
 	unitcell uc;
-	std::vector<cavity_magnet::site> sites;
-	std::vector<cavity_magnet::bond> bonds;
+	std::vector<double> Js;
 };
 
 static cavity_magnet_proto make_square(const loadl::parser &p) {
 	double J = p.get<double>("J");
 	double Jx = p.get<double>("Jx", J);
-	int spin_dim = round(2 * p.get<double>("S", 0.5) + 1);
 
-	const unitcell uc = unitcells::square;
-	auto mode_couplings = make_cavity(uc, p);
-
-	return {unitcells::square, {{spin_dim}}, {{Jx, mode_couplings[0]}, {J, mode_couplings[1]}}};
+	return {unitcells::square, {Jx, J}};
 }
 
 static cavity_magnet_proto make_honeycomb(const loadl::parser &p) {
 	double J = p.get<double>("J");
 	double Jx = p.get<double>("Jx", J);
-	int spin_dim = round(2 * p.get<double>("S", 0.5) + 1);
 
-	const auto &uc = unitcells::honeycomb;
+	return {unitcells::honeycomb, {Jx, J, J}};
+}
 
-	std::vector<std::vector<double>> mode_couplings = make_cavity(uc, p);
+static cavity_magnet_proto make_columnar_dimer(const loadl::parser &p) {
+	double J = p.get<double>("J");
+	double JD = p.get<double>("JD", J);
+	double Jx = p.get<double>("Jx", J);
 
-	std::vector<cavity_magnet::bond> bonds(uc.bonds.size());
-	std::transform(mode_couplings.begin(), mode_couplings.end(), bonds.begin(),
-	               [J](const auto &mc) {
-		               return cavity_magnet::bond{J, mc};
-	               });
-	bonds[0].J = Jx;
-
-	return {unitcells::honeycomb, {{spin_dim}, {spin_dim}}, bonds};
+	return {unitcells::honeycomb, {JD, Jx, Jx, J}};
 }
 
 std::unique_ptr<cavity_magnet> cavity_magnet_from_param(const loadl::parser &p) {
@@ -104,10 +95,23 @@ std::unique_ptr<cavity_magnet> cavity_magnet_from_param(const loadl::parser &p) 
 		proto = make_square(p);
 	} else if(lat == "honeycomb") {
 		proto = make_honeycomb(p);
+	} else if(lat == "columnar_dimer") {
+		proto = make_columnar_dimer(p);
 	} else {
 		throw std::runtime_error{fmt::format("unknown lattice '{}'", lat)};
 	}
 
-	return std::make_unique<cavity_magnet>(lattice{proto.uc, Lx, Ly}, modes, proto.sites,
-	                                       proto.bonds, U, cavity_magnet_measurement_settings{p});
+	int spin_dim = round(2 * p.get<double>("S", 0.5) + 1);
+	std::vector<cavity_magnet::site> sites(proto.uc.sites.size());
+	std::fill(sites.begin(), sites.end(), cavity_magnet::site{spin_dim});
+
+	auto mode_couplings = make_cavity(proto.uc, p);
+	std::vector<cavity_magnet::bond> bonds(proto.uc.bonds.size());
+	std::transform(mode_couplings.begin(), mode_couplings.end(), proto.Js.begin(), bonds.begin(),
+	               [](const auto &mc, double J) {
+		               return cavity_magnet::bond{J, mc};
+	               });
+
+	return std::make_unique<cavity_magnet>(lattice{proto.uc, Lx, Ly}, modes, sites, bonds, U,
+	                                       cavity_magnet_measurement_settings{p});
 }
