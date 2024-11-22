dafe411c6e48a6177af7c326dfb48ca8cd7f3881
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2024-11-22 12:23:53 -0500

    make sure seeded tests actually contain results…

--- test/gen_seeded_job.sh
+++ test/gen_seeded_job.sh
@@ -4,5 +4,7 @@ if [ $# -ne 2 ]; then
 	echo Usage: $0 JOBFILE SEEDED_JOBFILE
 	exit 1
 fi
-sed $1 -e 's/tm\.sweeps.*$/tm.sweeps = 50\ntm.seed = 18088206/g' -e 's/tm\.thermalization.*$/tm.thermalization = 50/g' > $2
+sed $1 -e 's/tm\.sweeps.*$/tm.sweeps = 50\ntm.seed = 18088206/g' \
+       -e 's/tm\.thermalization.*$/tm.thermalization = 50/g' \
+       -e 's/tm\.binsize.*$/tm.binsize = 1/g' > $2
 chmod +x $2
