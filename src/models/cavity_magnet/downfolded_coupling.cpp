5379c23ddef466edeaacfae29abe75196a7e30d1
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2024-11-22 12:24:12 -0500

    cavity_magnet: implement downfolded_coupling

---
+++ src/models/cavity_magnet/downfolded_coupling.cpp
@@ -0,0 +1,41 @@
+#include "downfolded_coupling.h"
+#include <cmath>
+#include <complex>
+
+using cmplx = std::complex<double>;
+using namespace std::complex_literals;
+
+static cmplx dispOp(int n, int l, double g) {
+	cmplx sum = 0;
+
+	for(int a = 0; a <= std::min(n,l); a++) {
+		cmplx term = std::pow(1i * g, n + l - 2*a)*exp(0.5*lgamma(1+l)+ 0.5*lgamma(1+n)-lgamma(1+a)-lgamma(1+n-a)-lgamma(1+l-a));
+		sum += term;
+	}
+	return sum;
+}
+
+double J(int n, int m, double omega, double g, double tolerance = 1e-12) {
+	if((n + m) % 2 != 0) {
+		return 0;
+	}
+	double sum = 0;
+	double term = 0;
+	for(int l = 0; l < std::max(n,m)+2 || fabs(term) > tolerance; l++) {
+		term = (dispOp(n,l,g) * std::conj(dispOp(m,l,g))).real() * (1.0/(1.0+omega*(l-n)) + 1.0/(1.0+omega*(l-m)));
+		sum += term;
+	}
+
+	return 0.5*exp(-g*g)*sum;
+}
+
+Eigen::MatrixXd downfolded_coupling(double omega, double g, int max_bosons) {
+	Eigen::MatrixXd coupling(max_bosons, max_bosons);
+	for(int m = 0; m < max_bosons; m++) {
+		for(int n = 0; n < max_bosons; n++) {
+			coupling(m,n) = J(n,m,omega,g);
+		}
+	}
+
+	return coupling;
+}
---
+++ test/test_cavity_magnet.cpp
@@ -0,0 +1,15 @@
+#include <catch.hpp>
+
+#include "models/cavity_magnet/downfolded_coupling.cpp"
+
+TEST_CASE("coupling evaluation") {
+	REQUIRE(dispOp(0,4,0.2).real() == Approx(0.0003265986323710905));
+	REQUIRE(dispOp(2,4,0.2).real() == Approx(-0.06744374904565532));
+	REQUIRE(dispOp(2,3,0.2).imag() == Approx(0.3326461310962948));
+
+	CHECK(J(1,1,0.55, 0.5) == Approx(1.0951812739150684));
+	CHECK(J(3,3,0.23, 0.2) == Approx(1.005934546456162));
+	CHECK(J(0,3,0.55, 0.5) == 0);
+	CHECK(J(1,3,0.23, 0.2) == Approx(-0.007657606067842092));
+	CHECK(J(3,1,0.23, 0.2) == Approx(J(3,1,0.23,0.2)));
+}
