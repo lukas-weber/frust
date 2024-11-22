09393c1734738d2b219c926273c2c33738ff8b5b
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2024-11-22 12:23:40 -0500

    test formatting

--- test/test_util.cpp
+++ test/test_util.cpp
@@ -1,10 +1,9 @@
-#include <catch2/catch.hpp>
 #include "util.h"
+#include <catch2/catch.hpp>
 
 TEST_CASE("init_mat") {
-	Eigen::MatrixXd a(3,3);
+	Eigen::MatrixXd a(3, 3);
 	a << 0, 1, 2, 3, 4, 5, 6, 7, 8;
 
-	REQUIRE(a == init_mat(3,3,{0,1,2,3,4,5,6,7,8}));
+	REQUIRE(a == init_mat(3, 3, {0, 1, 2, 3, 4, 5, 6, 7, 8}));
 }
-	
