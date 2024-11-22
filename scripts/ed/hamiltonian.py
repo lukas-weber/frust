c46e4ed29025ae811514693f2a4b9f83ca89d02b
Lukas Weber <49278367+lukas-weber@users.noreply.github.com>
2024-11-22 12:24:11 -0500

    ed: add arbitrary spin magnitudes for cavity magnet

--- scripts/ed/hamiltonian.py
+++ scripts/ed/hamiltonian.py
@@ -2,44 +2,49 @@ import numpy as np
 import scipy.sparse as sps
 import scipy.linalg as spl
 
+
+def spinop(component, spin_dim):
+    S = (spin_dim - 1) * 0.5
+    m = S - np.arange(spin_dim)
+    if component < 2:
+        splus = sps.diags([np.sqrt(S * (S + 1) - m[1:] * (m[1:] + 1))], offsets=[1])
+        if component == 0:
+            return (splus + splus.H) * 0.5
+        return -(splus - splus.H) * 0.5j
+    if component == 2:
+        return sps.diags([m], [0])
+
+
 σz2 = np.array([[1, 0], [0, -1]])
 σy2 = np.array([[0, -1j], [1j, 0]])
 σx2 = np.array([[0, 1], [1, 0]])
 
 
-def lift_op(pos, op, N):
-    return sps.kron(
-        sps.kron(sps.identity(2 ** (pos)), op), sps.identity(2 ** (N - pos - 1))
-    )
+class Lifter:
+    def __init__(self, site_dims):
+        self.site_dims = site_dims
+        self.cum_site_dims = np.append([1], np.cumprod(site_dims))
+
+    def lift_op(self, pos, op):
+        assert all(d == self.site_dims[pos] for d in op.shape)
+        return sps.kron(
+            sps.kron(sps.identity(self.cum_site_dims[pos]), op),
+            sps.identity(self.cum_site_dims[-1] / self.cum_site_dims[pos + 1]),
+        )
 
 
-def Sx(pos, N):
-    return lift_op(pos, σx2 / 2, N)
+class SpinLifter(Lifter):
+    def __init__(self, site_dims):
+        Lifter.__init__(self, site_dims)
 
+    def Sa(self, a, pos):
+        return self.lift_op(pos, spinop(a, self.site_dims[pos]))
 
-def Sy(pos, N):
-    return lift_op(pos, σy2 / 2, N)
+    def Sz(self, pos):
+        return self.Sa(2, pos)
 
-
-def Sz(pos, N):
-    return lift_op(pos, σz2 / 2, N)
-
-
-def heisen_bond(i, j, N):
-    return (Sx(i, N) @ Sx(j, N) + Sy(i, N) @ Sy(j, N) + Sz(i, N) @ Sz(j, N)).real
-
-
-def l_operator(n):
-    S2 = sum(
-        Sx(i, n) * Sx(j, n) + Sy(i, n) * Sy(j, n) + Sz(i, n) * Sz(j, n)
-        for i in range(n)
-        for j in range(n)
-    ).real.todense()
-
-    w, v = np.linalg.eigh(S2)
-    assert np.allclose(v @ np.diag(w) @ v.T, S2)
-
-    return v @ np.diag(np.sqrt(w + 0.25) - 0.5) @ v.T
+    def heisen_bond(self, i, j):
+        return sum(self.Sa(a, i) @ self.Sa(a, j) for a in range(3)).real
 
 
 class Ensemble:
